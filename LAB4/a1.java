7b4c2f9629e4b00acbc68a83afccf368784bb4ab
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-24 22:29:46 +0530

    Add files via upload

---
+++ LAB4/a1.java
@@ -0,0 +1,24 @@
+package pack;  
+  
+public class a1 {  
+  
+protected void msg1()  
+ {  
+     System.out.println("msg1");  
+ }  
+
+ protected void msg2()  
+ {  
+     System.out.println("msg2");  
+ }  
+
+ protected void msg3()  
+ {  
+     System.out.println("msg3");  
+ }  
+ protected void msg4()  
+ {  
+     System.out.println("msg4");  
+ }  
+       
+}  
---
+++ LAB4/balance.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class balance {
+
+    public void display(){
+        System.out.println("YOUR FINAL BALANCE IS 5000 RUPEE ");
+    }
+    
+}
