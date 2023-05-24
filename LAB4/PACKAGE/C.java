3ddc5c9cf2fb184e23ff315c83f71a8735f96229
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-24 22:29:11 +0530

    Add files via upload

---
+++ LAB4/PACKAGE/C.java
@@ -0,0 +1,10 @@
+package LAB.lab6;
+import pack.*;
+
+class C {
+    public static void main(String args[]){  
+        balance obj = new balance();  
+        obj.display();
+    
+}
+}
---
+++ LAB4/PACKAGE/b1.java
@@ -0,0 +1,11 @@
+package LAB.lab6;
+
+import pack.a1;  
+  
+public class b1 extends a1 {  
+public static void main(String[] args) {  
+    a1 a = new a1();  
+    a.msg1();
+ 
+}  
+} 
