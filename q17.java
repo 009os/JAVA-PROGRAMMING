7a24444cff4bb750cd681d9ceac535d1d5876924
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-24 22:30:39 +0530

    Add files via upload

---
+++ q17.java
@@ -0,0 +1,15 @@
+package LAB.lab7;
+public class q17 {
+    public static void main(String[] args) {
+        String[] names = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        int[] rollNumbers = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+
+        try {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println("Name: " + names[i] + ", Roll Number: " + rollNumbers[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Out Of Bounds Exception: " + e.getMessage());
+        }
+    }
+}
